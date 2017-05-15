cf9781af007571bd00ddcb7b27424ffe28ecd552
pebble8888 <pebble8888@gmail.com>
2017-05-15 13:19:00 +0900

    clean

--- Pods/Target Support Files/Pods-Ed25519iOS/Pods-Ed25519iOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Ed25519iOS : NSObject
-@end
-@implementation PodsDummy_Pods_Ed25519iOS
-@end
--- Pods/Target Support Files/Pods-Ed25519iOSTests/Pods-Ed25519iOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Ed25519iOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_Ed25519iOSTests
-@end
