ea02de9aa97dea9a6d5b6501b5f0ad662e540911
pebble8888 <pebble8888@gmail.com>
2019-03-22 14:14:02 +0900

    improve codeing style

--- Ed25519macOS/ed25519_ge.swift
+++ Ed25519macOS/ed25519_ge.swift
@@ -38,102 +38,101 @@ import Foundation
 // -> (XZ, YZ, XY, Z^2)
 // 
 struct ge: CustomDebugStringConvertible {
-    var x:fe
-    var y:fe
-    var z:fe
-    var t:fe
-    public var debugDescription:String {
+    var x: fe
+    var y: fe
+    var z: fe
+    var t: fe
+    public var debugDescription: String {
         return "x:\(x)\n" + "y:\(y)\n" + "z:\(z)\n" + "t:\(t)\n"
     }
-    
-    init(){
+
+    init() {
         x = fe() // zero
         y = fe() // zero
         z = fe() // zero
         t = fe() // zero
     }
-    
-    init(_ x:fe, _ y:fe, _ z:fe, _ t:fe){
+
+    init(_ x: fe, _ y: fe, _ z: fe, _ t: fe) {
         self.x = x
         self.y = y
         self.z = z
         self.t = t
     }
 
-    private var toP2:P2 {
+    private var toP2: P2 {
         return P2(self.x, self.y, self.z)
     }
-    
-    mutating private func setFromP2(_ p2:P2){
+
+    mutating private func setFromP2(_ p2: P2) {
         x = p2.x
         y = p2.y
         z = p2.z
     }
-   
+
     /* 
      * Arithmetic on the twisted Edwards curve -x^2 + y^2 = 1 + dx^2y^2 
      * with d = -(121665/121666) = 37095705934669439343138083508754565189542113879843219016388785533085940283555
      * Base point: (15112221349535400772501151409588531511454012693041857206046113283949847762202,46316835694926478169428394003475163141307993866256225615783033603165251855960)
      */
-    
+
     /* d */
-    private static let ecd:fe = fe([0xA3, 0x78, 0x59, 0x13, 0xCA, 0x4D, 0xEB, 0x75, 0xAB, 0xD8, 0x41, 0x41, 0x4D, 0x0A, 0x70, 0x00,
+    private static let ecd: fe = fe([0xA3, 0x78, 0x59, 0x13, 0xCA, 0x4D, 0xEB, 0x75, 0xAB, 0xD8, 0x41, 0x41, 0x4D, 0x0A, 0x70, 0x00,
     0x98, 0xE8, 0x79, 0x77, 0x79, 0x40, 0xC7, 0x8C, 0x73, 0xFE, 0x6F, 0x2B, 0xEE, 0x6C, 0x03, 0x52])
     /* 2*d */
-    private static let ec2d:fe = fe([0x59, 0xF1, 0xB2, 0x26, 0x94, 0x9B, 0xD6, 0xEB, 0x56, 0xB1, 0x83, 0x82, 0x9A, 0x14, 0xE0, 0x00,
+    private static let ec2d: fe = fe([0x59, 0xF1, 0xB2, 0x26, 0x94, 0x9B, 0xD6, 0xEB, 0x56, 0xB1, 0x83, 0x82, 0x9A, 0x14, 0xE0, 0x00,
     0x30, 0xD1, 0xF3, 0xEE, 0xF2, 0x80, 0x8E, 0x19, 0xE7, 0xFC, 0xDF, 0x56, 0xDC, 0xD9, 0x06, 0x24])
     /* sqrt(-1) = 2^((p-1)/4) */
-    private static let sqrtm1:fe = fe([0xB0, 0xA0, 0x0E, 0x4A, 0x27, 0x1B, 0xEE, 0xC4, 0x78, 0xE4, 0x2F, 0xAD, 0x06, 0x18, 0x43, 0x2F,
+    private static let sqrtm1: fe = fe([0xB0, 0xA0, 0x0E, 0x4A, 0x27, 0x1B, 0xEE, 0xC4, 0x78, 0xE4, 0x2F, 0xAD, 0x06, 0x18, 0x43, 0x2F,
     0xA7, 0xD7, 0xFB, 0x3D, 0x99, 0x00, 0x4D, 0x2B, 0x0B, 0xDF, 0xC1, 0x4F, 0x80, 0x24, 0x83, 0x2B])
 
     // point
     private struct P1P1 {
-        var e:fe
-        var h:fe
-        var g:fe
-        var f:fe
-        init(){
+        var e: fe
+        var h: fe
+        var g: fe
+        var f: fe
+        init() {
             e = fe()
             h = fe()
             g = fe()
             f = fe()
         }
-    } 
-	
+    }
+
     // projective coordinates
     private struct P2 {
-        var x:fe
-        var y:fe
-        var z:fe
-        init(){
+        var x: fe
+        var y: fe
+        var z: fe
+        init() {
             x = fe()
             y = fe()
             z = fe()
         }
-        init(_ x:fe, _ y:fe, _ z:fe){
+        init(_ x: fe, _ y: fe, _ z: fe) {
             self.x = x
             self.y = y
             self.z = z
         }
     }
-	
+
 	// affine coordinate point
-    private struct aff
-    {
-        var x:fe
-        var y:fe
-        init(){
+    private struct aff {
+        var x: fe
+        var y: fe
+        init() {
             x = fe()
             y = fe()
         }
-        init(_ x:fe, _ y:fe){
+        init(_ x: fe, _ y: fe) {
             self.x = x
             self.y = y
         }
     }
-    
+
     /* Packed coordinates of the base point */
-    static let ge25519_base:ge = ge(
+    static let ge25519_base: ge = ge(
     fe([0x1A, 0xD5, 0x25, 0x8F, 0x60, 0x2D, 0x56, 0xC9, 0xB2, 0xA7, 0x25, 0x95, 0x60, 0xC7, 0x2C, 0x69,
         0x5C, 0xDC, 0xD6, 0xFD, 0x31, 0xE2, 0xA4, 0xC0, 0xFE, 0x53, 0x6E, 0xCD, 0xD3, 0x36, 0x69, 0x21]),
     fe([0x58, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66,
@@ -147,36 +146,33 @@ struct ge: CustomDebugStringConvertible {
     // y = H G
     // z = F G
 	// (t = E H)
-    private static func p1p1_to_p2(_ r:inout P2, _ p:P1P1)
-    {
+    private static func p1p1_to_p2(_ r: inout P2, _ p: P1P1) {
         fe.fe25519_mul(&r.x, p.e, p.f)
         fe.fe25519_mul(&r.y, p.g, p.h)
         fe.fe25519_mul(&r.z, p.f, p.g)
     }
-    
+
     // 
-    private static func p1p1_to_p3(_ r:inout ge, _ p:P1P1)
-    {
+    private static func p1p1_to_p3(_ r: inout ge, _ p: P1P1) {
         var p2 = P2()
         p1p1_to_p2(&p2, p)
         r.setFromP2(p2)
         // t = E H
         fe.fe25519_mul(&r.t, p.e, p.h)
     }
-    
-    private static func ge25519_mixadd2(_ r: inout ge, _ q:aff)
-    {
-        var a:fe = fe()
-        var b:fe = fe()
-        var t1:fe = fe()
-        var t2:fe = fe()
-        var c:fe = fe()
-        var d:fe = fe()
-        var e:fe = fe()
-        var f:fe = fe()
-        var g:fe = fe()
-        var h:fe = fe()
-        var qt:fe = fe()
+
+    private static func ge25519_mixadd2(_ r: inout ge, _ q: aff) {
+        var a = fe()
+        var b = fe()
+        var t1 = fe()
+        var t2 = fe()
+        var c = fe()
+        var d = fe()
+        var e = fe()
+        var f = fe()
+        var g = fe()
+        var h = fe()
+        var qt = fe()
         fe.fe25519_mul(&qt, q.x, q.y) /* t = x y */
         fe.fe25519_sub(&a, r.y, r.x) /* A = Ry-Rx */
         fe.fe25519_add(&b, r.y, r.x) /* B = Ry+Rx */
@@ -200,14 +196,13 @@ struct ge: CustomDebugStringConvertible {
     }
 
     // r = p (E coordinates) + q (E coordinates)
-    private static func add_p1p1(_ r: inout ge.P1P1, _ p:ge, _ q:ge)
-    {
+    private static func add_p1p1(_ r: inout ge.P1P1, _ p: ge, _ q: ge) {
         var a = fe()
         var b = fe()
         var c = fe()
         var d = fe()
         var t = fe()
-    
+
         fe.fe25519_sub(&a, p.y, p.x) /* a = y1-x1 */
         fe.fe25519_sub(&t, q.y, q.x) /* t = y2-x2 */
         fe.fe25519_mul(&a, a, t)     /* A = (y1-x1)(y2-x2) */
@@ -227,11 +222,10 @@ struct ge: CustomDebugStringConvertible {
         fe.fe25519_add(&r.g, d, c)   /* G = D+C */
         fe.fe25519_add(&r.h, b, a)   /* H = B+A */
     }
-    
+
     // r = 2 * p
     // projective coordinate doubling 
-    private static func dbl_p1p1(_ r:inout P1P1, _ p:P2)
-    {
+    private static func dbl_p1p1(_ r: inout P1P1, _ p: P2) {
         var a = fe()
         var b = fe()
         var c = fe()
@@ -241,7 +235,7 @@ struct ge: CustomDebugStringConvertible {
         fe.fe25519_square(&c, p.z) /* c = z^2 */ 
         fe.fe25519_add(&c, c, c)   /* C = 2 z^2 */
         fe.fe25519_neg(&d, a)      /* D = - x^2 */
-        
+
         fe.fe25519_add(&r.e, p.x, p.y) /* e = x+y */
         fe.fe25519_square(&r.e, r.e)   /* e = (x+y)^2 */
         fe.fe25519_sub(&r.e, r.e, a)   /* e = (x+y)^2 - x^2 */
@@ -253,63 +247,57 @@ struct ge: CustomDebugStringConvertible {
 
         fe.fe25519_sub(&r.h, d, b)     /* H = - x^2 - y^2 */
     }
-    
+
     /* Constant-time version of: if(b) r = p */
-    private static func cmov_aff(_ r:inout aff, _ p:aff, _ b:UInt8)
-    {
+    private static func cmov_aff(_ r: inout aff, _ p: aff, _ b: UInt8) {
         fe.fe25519_cmov(&r.x, p.x, b)
         fe.fe25519_cmov(&r.y, p.y, b)
     }
-    
-    private static func equal(_ b:Int8, _ c:Int8) -> UInt8
-    {
+
+    private static func equal(_ b: Int8, _ c: Int8) -> UInt8 {
         return b == c ? 1 : 0
     }
-    
+
     // retval  1: negative
     // retval  0: zero or positive
-    private static func negative(_ b:Int8) -> UInt8
-    {
+    private static func negative(_ b: Int8) -> UInt8 {
         return b < 0 ? 1 : 0
     }
-	
+
 	// t
 	// pos:
 	// b:
-    private static func choose_t(_ t:inout aff, _ pos:Int, _ b:Int8)
-    {
+    private static func choose_t(_ t: inout aff, _ pos: Int, _ b: Int8) {
         /* constant time */
         var v = fe()
         t = ge25519_base_multiples_affine.shared.v[5*pos+0]
-        cmov_aff(&t, ge25519_base_multiples_affine.shared.v[5*pos+1],equal(b,1) | equal(b,-1))
-        cmov_aff(&t, ge25519_base_multiples_affine.shared.v[5*pos+2],equal(b,2) | equal(b,-2))
-        cmov_aff(&t, ge25519_base_multiples_affine.shared.v[5*pos+3],equal(b,3) | equal(b,-3))
-        cmov_aff(&t, ge25519_base_multiples_affine.shared.v[5*pos+4],equal(b,-4))
+        cmov_aff(&t, ge25519_base_multiples_affine.shared.v[5*pos+1], equal(b, 1) | equal(b, -1))
+        cmov_aff(&t, ge25519_base_multiples_affine.shared.v[5*pos+2], equal(b, 2) | equal(b, -2))
+        cmov_aff(&t, ge25519_base_multiples_affine.shared.v[5*pos+3], equal(b, 3) | equal(b, -3))
+        cmov_aff(&t, ge25519_base_multiples_affine.shared.v[5*pos+4], equal(b, -4))
         fe.fe25519_neg(&v, t.x)
         fe.fe25519_cmov(&t.x, v, negative(b))
     }
-	
-    private static func setneutral(_ r:inout ge)
-    {
+
+    private static func setneutral(_ r: inout ge) {
         fe.fe25519_setzero(&r.x)
         fe.fe25519_setone(&r.y)
         fe.fe25519_setone(&r.z)
         fe.fe25519_setzero(&r.t)
     }
-    
+
     /**
      * EXPORTED FUNCTIONS
      */
 
     /* return true on success, false otherwise */
 	// @warning  result is negated
-    static func ge25519_unpackneg_vartime(_ r:inout ge, _ p:[UInt8] /* 32 */) -> Bool
-    {
+    static func ge25519_unpackneg_vartime(_ r: inout ge, _ p: [UInt8] /* 32 */) -> Bool {
 		// x^2 = a mod q
 		// 1) x1 = uv^3 (uv^7)^((q-5)/8) or
 		// 2) x2 = uv^3 (uv^7)^((q-5)/8) 2^((q-1)/4)
 		assert(p.count == 32)
-        var par:UInt8
+        var par: UInt8
         var t = fe()
         var chk = fe()
         var u = fe()
@@ -325,19 +313,19 @@ struct ge: CustomDebugStringConvertible {
         fe.fe25519_mul(&v, u, ge.ecd) /* v = dy^2 */
         fe.fe25519_sub(&u, u, r.z) /* u = y^2-1 */
         fe.fe25519_add(&v, r.z, v) /* v = dy^2+1 */
-        
+
         fe.fe25519_square(&den2, v)
         fe.fe25519_square(&den4, den2)
         fe.fe25519_mul(&den6, den4, den2)
         fe.fe25519_mul(&t, den6, u)
         fe.fe25519_mul(&t, t, v)
         fe.fe25519_pow2523(&t, t) // t = (uv^7)^((q-5)/8)
-		
+
         fe.fe25519_mul(&t, t, u) // t = u (uv^7)^((q-5)/8)
         fe.fe25519_mul(&t, t, v)
         fe.fe25519_mul(&t, t, v)
         fe.fe25519_mul(&r.x, t, v) // t = uv^3 (uv^7)^((q-5)/8)
-        
+
 		// x^2 = u/v
 		// x^2 v = u
         fe.fe25519_square(&chk, r.x) // (r.x)^2
@@ -345,32 +333,30 @@ struct ge: CustomDebugStringConvertible {
         if !fe.fe25519_iseq_vartime(chk, u) {
             fe.fe25519_mul(&r.x, r.x, ge.sqrtm1)
         }
-		
+
 		/* check (r.x)^2 = u/v */
         fe.fe25519_square(&chk, r.x)
         fe.fe25519_mul(&chk, chk, v)
         if !fe.fe25519_iseq_vartime(chk, u) {
             return false
         }
-		
+
 		// @warning negated
         if fe.fe25519_getparity(r.x) != (1-par) {
             fe.fe25519_neg(&r.x, r.x)
         }
-        
+
         fe.fe25519_mul(&r.t, r.x, r.y)
         return true
     }
-	
+
 	/// @note  result is freezed
-	static func ge25519_negate(_ r:inout ge)
-	{
+	static func ge25519_negate(_ r: inout ge) {
 		fe.fe25519_neg(&r.x, r.x)
 		fe.fe25519_neg(&r.t, r.t)
 	}
 
-    static func ge25519_pack(_ r:inout [UInt8] /* 32 or more */, _ p:ge)
-    {
+    static func ge25519_pack(_ r: inout [UInt8] /* 32 or more */, _ p: ge) {
 		assert(r.count >= 32)
         var tx = fe()
         var ty = fe()
@@ -383,89 +369,82 @@ struct ge: CustomDebugStringConvertible {
         r[31] ^= par
     }
 
-    static func ge25519_isneutral_vartime(_ p:ge) -> Int32
-    {
-        var ret:Int32 = 1
-        if(!fe.fe25519_iszero(p.x)) { ret = 0 }
-        if(!fe.fe25519_iseq_vartime(p.y, p.z)) { ret = 0 }
+    static func ge25519_isneutral_vartime(_ p: ge) -> Int32 {
+        var ret: Int32 = 1
+        if (!fe.fe25519_iszero(p.x)) { ret = 0 }
+        if (!fe.fe25519_iseq_vartime(p.y, p.z)) { ret = 0 }
         return ret
     }
 
     /* computes [s1]p1 + [s2]p2 */
-    static func ge25519_double_scalarmult_vartime(_ r:inout ge, _ p1:ge, _ s1:sc, _ p2:ge, _ s2:sc)
-    {
+    static func ge25519_double_scalarmult_vartime(_ r: inout ge, _ p1: ge, _ s1: sc, _ p2: ge, _ s2: sc) {
         var tp1p1 = ge.P1P1()
-        var pre:[ge] = [ge](repeating:ge(), count:16)
-        var b:[UInt8] = [UInt8](repeating:0, count:127)
-        
+        var pre: [ge] = [ge](repeating: ge(), count: 16)
+        var b: [UInt8] = [UInt8](repeating: 0, count: 127)
+
         /* precomputation                                                          s2 s1 */
         ge.setneutral(&pre[0])                                                  /* 00 00 */
         pre[1] = p1                                                             /* 00 01 */
-        ge.dbl_p1p1(&tp1p1,p1.toP2);             ge.p1p1_to_p3( &pre[2], tp1p1) /* 00 10 */
-        ge.add_p1p1(&tp1p1,pre[1], pre[2]);      ge.p1p1_to_p3( &pre[3], tp1p1) /* 00 11 */
+        ge.dbl_p1p1(&tp1p1, p1.toP2);             ge.p1p1_to_p3(&pre[2], tp1p1) /* 00 10 */
+        ge.add_p1p1(&tp1p1, pre[1], pre[2]);      ge.p1p1_to_p3(&pre[3], tp1p1) /* 00 11 */
         pre[4] = p2                                                             /* 01 00 */
-        ge.add_p1p1(&tp1p1,pre[1], pre[4]);      ge.p1p1_to_p3( &pre[5], tp1p1) /* 01 01 */
-        ge.add_p1p1(&tp1p1,pre[2], pre[4]);      ge.p1p1_to_p3( &pre[6], tp1p1) /* 01 10 */
-        ge.add_p1p1(&tp1p1,pre[3], pre[4]);      ge.p1p1_to_p3( &pre[7], tp1p1) /* 01 11 */
-        ge.dbl_p1p1(&tp1p1,p2.toP2);             ge.p1p1_to_p3( &pre[8], tp1p1) /* 10 00 */
-        ge.add_p1p1(&tp1p1,pre[1], pre[8]);      ge.p1p1_to_p3( &pre[9], tp1p1) /* 10 01 */
-        ge.dbl_p1p1(&tp1p1,pre[5].toP2);         ge.p1p1_to_p3(&pre[10], tp1p1) /* 10 10 */
-        ge.add_p1p1(&tp1p1,pre[3], pre[8]);      ge.p1p1_to_p3(&pre[11], tp1p1) /* 10 11 */
-        ge.add_p1p1(&tp1p1,pre[4], pre[8]);      ge.p1p1_to_p3(&pre[12], tp1p1) /* 11 00 */
-        ge.add_p1p1(&tp1p1,pre[1],pre[12]);      ge.p1p1_to_p3(&pre[13], tp1p1) /* 11 01 */
-        ge.add_p1p1(&tp1p1,pre[2],pre[12]);      ge.p1p1_to_p3(&pre[14], tp1p1) /* 11 10 */
-        ge.add_p1p1(&tp1p1,pre[3],pre[12]);      ge.p1p1_to_p3(&pre[15], tp1p1) /* 11 11 */
-        
-        sc.sc25519_2interleave2(&b,s1,s2)
-        
+        ge.add_p1p1(&tp1p1, pre[1], pre[4]);      ge.p1p1_to_p3(&pre[5], tp1p1) /* 01 01 */
+        ge.add_p1p1(&tp1p1, pre[2], pre[4]);      ge.p1p1_to_p3(&pre[6], tp1p1) /* 01 10 */
+        ge.add_p1p1(&tp1p1, pre[3], pre[4]);      ge.p1p1_to_p3(&pre[7], tp1p1) /* 01 11 */
+        ge.dbl_p1p1(&tp1p1, p2.toP2);             ge.p1p1_to_p3(&pre[8], tp1p1) /* 10 00 */
+        ge.add_p1p1(&tp1p1, pre[1], pre[8]);      ge.p1p1_to_p3(&pre[9], tp1p1) /* 10 01 */
+        ge.dbl_p1p1(&tp1p1, pre[5].toP2);         ge.p1p1_to_p3(&pre[10], tp1p1) /* 10 10 */
+        ge.add_p1p1(&tp1p1, pre[3], pre[8]);      ge.p1p1_to_p3(&pre[11], tp1p1) /* 10 11 */
+        ge.add_p1p1(&tp1p1, pre[4], pre[8]);      ge.p1p1_to_p3(&pre[12], tp1p1) /* 11 00 */
+        ge.add_p1p1(&tp1p1, pre[1], pre[12]);      ge.p1p1_to_p3(&pre[13], tp1p1) /* 11 01 */
+        ge.add_p1p1(&tp1p1, pre[2], pre[12]);      ge.p1p1_to_p3(&pre[14], tp1p1) /* 11 10 */
+        ge.add_p1p1(&tp1p1, pre[3], pre[12]);      ge.p1p1_to_p3(&pre[15], tp1p1) /* 11 11 */
+
+        sc.sc25519_2interleave2(&b, s1, s2)
+
         /* scalar multiplication */
         r = pre[Int(b[126])]
-        for i in stride(from:125, through:0, by: -1)
-        {
+        for i in stride(from: 125, through: 0, by: -1) {
             ge.dbl_p1p1(&tp1p1, r.toP2)
             var t = ge.P2()
             ge.p1p1_to_p2(&t, tp1p1)
             r.setFromP2(t)
-            
+
             ge.dbl_p1p1(&tp1p1, r.toP2)
-            if(b[i] != 0)
-            {
+            if (b[i] != 0) {
                 ge.p1p1_to_p3(&r, tp1p1)
                 ge.add_p1p1(&tp1p1, r, pre[Int(b[i])])
             }
-            if(i != 0) {
+            if (i != 0) {
                 var t = ge.P2()
                 ge.p1p1_to_p2(&t, tp1p1)
                 r.setFromP2(t)
-            }
-            else {
+            } else {
                 ge.p1p1_to_p3(&r, tp1p1)
             }
         }
     }
 
-    static func ge25519_scalarmult_base(_ r:inout ge, _ s:sc)
-    {
-        var b:[Int8] = [Int8](repeating:0, count:85)
-        var t:aff = ge.aff()
+    static func ge25519_scalarmult_base(_ r: inout ge, _ s: sc) {
+        var b: [Int8] = [Int8](repeating: 0, count: 85)
+        var t: aff = ge.aff()
         sc.sc25519_window3(&b, s)
-        
-        var tmpaff:aff = ge.aff()
+
+        var tmpaff: aff = ge.aff()
         ge.choose_t(&tmpaff, 0, b[0])
         r.x = tmpaff.x
         r.y = tmpaff.y
 
         fe.fe25519_setone(&r.z)
         fe.fe25519_mul(&r.t, r.x, r.y)
-        for i in 1..<85
-        {
+        for i in 1..<85 {
             ge.choose_t(&t, i, b[i])
             ge.ge25519_mixadd2(&r, t)
         }
     }
-	
+
 	private class ge25519_base_multiples_affine {
-		public var v:[aff] = []
+		public var v: [aff] = []
 		static let shared = ge25519_base_multiples_affine()
 
         /* Multiples of the base point in affine representation */
@@ -3021,7 +3000,7 @@ struct ge: CustomDebugStringConvertible {
                 fe([0x69, 0x3e, 0x47, 0x97, 0x2c, 0xaf, 0x52, 0x7c, 0x78, 0x83, 0xad, 0x1b, 0x39, 0x82, 0x2f, 0x02,
                     0x6f, 0x47, 0xdb, 0x2a, 0xb0, 0xe1, 0x91, 0x99, 0x55, 0xb8, 0x99, 0x3a, 0xa0, 0x44, 0x11, 0x51])
                 )
-            ) 
+            )
             if v.count != 85 * 5 {
                 assert(false)
             }
