9e837b809581b870ae673e2699f4b6312832d39b
Michal Kalinowski <ts-michal.kalinowski@rakuten.com>
2021-02-19 09:46:45 +0100

    add platforms information to Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "ed25519swift",
+    platforms: [
+        .macOS(.v10_12), .iOS("11.4")
+    ],
     products: [
         .library(
             name: "ed25519swift",
